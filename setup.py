4400db2cfeab22040653bbf6b7d4e0da030c9ff3
ollelindgren <lindgrenolle@live.se>
2021-07-18 17:37:47 +0200

    Version 0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements=[]
 
 setuptools.setup(
     name="datasystems",
-    version="v0.2.2",
+    version="v0.3",
     author="Olle Lindgren",
     author_email="lindgrenolle@live.se",
     description="A package for systematically managing data",
